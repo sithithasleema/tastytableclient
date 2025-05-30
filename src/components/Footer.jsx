0b62bf427e6929224b8f7be64b196709195f82b3
sithithasleema <sthaslima@gmail.com>
2025-05-30 14:33:15 +1000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className=" text-center py-2 fixed bottom-0 text-sm  w-full">
+    <div className=" text-center py-2  text-sm  w-full">
       &copy; {year}{" "}
       <a
         href="https://sithithasleema.github.io/sithi-portfolio-react/"
